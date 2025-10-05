0e4670071d68cbf3806f053dd9fe1a34ecfd1c27
Rio <riomaruyama@hotmail.com>
2025-10-04 17:44:52 -0700

    added wrapped garden with wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,9 @@ function App() {
 
         <Route path="/garden"
           element={
-            <Garden/>
+            <Wrapper>
+              <Garden/>
+            </Wrapper>
           }>
         </Route>
       </Routes>
--- src/components/Garden/Garden.jsx
+++ src/components/Garden/Garden.jsx
@@ -8,9 +8,39 @@ import { useState, useEffect } from "react"
 
 
 export const Garden = () => {
-    
+const [points, setPoints] = useState();
+    const flowers = {
+        flower1: {
+            pic: {flower1},
+            points: 20
+        },
+        flower2: {
+            pic: {flower2},
+            points: 40
+        },
+        flower3: {
+            pic: {flower3},
+            points: 60
+        },
+        flower4: {
+            pic: {flower4},
+            points: 80
+        }
+    };
+
+    useEffect(() => {
+        async function fetchPoints () {
+            const { data, error } = await supabase.from("user_points").select("total_points");
+            if (error) console.error(error);
+            else setPoints(data)
+        }
+        fetchPoints();
+    }, []);
+
+    const numAchievements = Math.floor(points / 20)
     return (
        <h1>My garden</h1>
+
 
     )
 }
