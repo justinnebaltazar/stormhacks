b458bbc1dd70be3a545a601d23ee076ab362f124
John Camino <jal.cmno@gmail.com>
2025-10-04 23:39:50 -0700

    Added Future works in Abous Us

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -16,11 +16,35 @@ export const About = () => {
       </div>
 
       <div className={styles.box}>
+        <h1 className={styles.title}>Future Works</h1>
+        <p className={styles.subtitle}> We want to preface that this project was built during a 24‑hour hackathon, so what you see is just the foundation of a much bigger vision. If we continue developing this project, here are some of the features we plan to add:</p>
+          <ul className={styles.list}>
+    <li>
+      <strong>Social:</strong> Allow users to interact with their friends on the app, whether it be visiting their gardens, trading seeds, or contributing to each other's gardens
+    </li>
+    <li>
+      <strong>More Integrations:</strong> Connect fitness apps or transit apps to automatically log activities
+    </li>
+    <li>
+      <strong>Goals:</strong> Set personal sustainability goals every week and track progress
+    </li>
+    <li>
+      <strong>Educational:</strong> Provide sustainability facts or tips to spread awareness
+    </li>
+    <li>
+      <strong>Community Awareness:</strong> Explore integrations with real-world sustainability initiatives and inform users with sustainable projects around the area
+    </li>
+  </ul>
+
+
+      </div>
+
+      <div className={styles.box}>
         <h1 className={styles.title}>Contact Us</h1>
         <p className={styles.subtitle}>
-        <p>Justinne Baltazar: <a href = "https://www.linkedin.com/in/justinnebaltazar/"> LinkedIn </a></p>
-        <p>John Camino: <a href = "https://www.linkedin.com/in/john-aldrix-camino/">LinkedIn</a></p>
-        <p>Rio Maruyama: <a href = "https://www.linkedin.com/in/rio-maruyama/"> LinkedIn </a> </p>
+        <p>Justinne Baltazar: <a href = "https://www.linkedin.com/in/justinnebaltazar/">LinkedIn</a> <a href="https://github.com/justinnebaltazar">GitHub</a></p>
+        <p>John Camino: <a href = "https://www.linkedin.com/in/john-aldrix-camino/">LinkedIn</a> <a href = "https://github.com/jcmno"> GitHub</a></p>
+        <p>Rio Maruyama: <a href = "https://www.linkedin.com/in/rio-maruyama/">LinkedIn</a> <a href ="https://github.com/maruyaya"> GitHub</a> </p>
         </p>
       </div>
     </div>
